778474cdfd6c79fd96216f11ef9c1a35e6889d00
Jack Case <jackacase@gmail.com>
2025-04-16 20:03:54 +0000

    start db.rs

--- rustseq/src/block.rs
+++ rustseq/src/block.rs
@@ -1,25 +1,49 @@
-#[derive(Debug)]
-pub struct Block {
+/// a BlockRow is a direct translation of a block row from the 
+/// database. It will be used to create a tree of Block objects
+/// in memory.
+pub struct BlockRow {
+    id: u32,
     content: String,
 
-    parent: Option<&Block>,
-    next_sibling: Option<&Block>
+    parent_id: Option<u32>,
+    sibling_id: Option<u32>,
 }
 
-impl Block {
-    pub fn new(content: &str) -> Self {
+impl BlockRow {
+    pub fn new(id: u32, content: &str, parent_id: Option<u32>, sibling_id: Option<u32>) -> Self {
+        BlockRow {
+            id: id,
+            content: String::from(content),
+            parent_id: parent_id,
+            sibling_id: sibling_id
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Block<'a, 'b> {
+    id: u32,
+    content: String,
+
+    parent: Option<&'a Block<'a,'b>>,
+    next_sibling: Option<&'b Block<'a,'b>>
+}
+
+impl<'a, 'b> Block<'a, 'b> {
+    pub fn new(id: u32, content: &str) -> Self {
         Block {
+            id: id,
             content: String::from(content),
             parent: Option::None,
             next_sibling: Option::None
         }
     }
 
-    pub fn set_parent(&self, parent: &Block) {
+    pub fn set_parent(&mut self, parent: &'a Block) {
         self.parent = Option::Some(parent);
     }
 
-    pub fn set_sibling(&self, sibling: &Block) {
+    pub fn set_sibling(&mut self, sibling: &'b Block) {
         self.next_sibling = Option::Some(sibling);
     }
 }
--- rustseq/src/main.rs
+++ rustseq/src/main.rs
@@ -1,14 +1,18 @@
 mod block;
+mod db;
 
 use block::Block;
 
 fn main() {
     println!("Hello, world!");
 
-    let block1 = Block::new("Hello, world!");
-    let block2 = Block::new("This is a child block");
+    let mut block1 = Block::new(1, "Hello, world!");
+    let mut block2 = Block::new(2, "This is a child block");
+    let mut block3 = Block::new(3, "this is a sibling");
 
     block2.set_parent(&block1);
+    block3.set_parent(&block1);
+    block2.set_sibling(&block3);
 
-    println!("{:?}", block1);
+    println!("{:?}", block2);
 }
