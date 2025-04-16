8c844085f658527a322c5379b187b2d270fd8b9a
Jack Case <jackacase@gmail.com>
2025-04-16 20:33:25 +0000

    ah, quotes are optional

--- rustseq/src/db.rs
+++ rustseq/src/db.rs
@@ -13,14 +13,15 @@ impl DB {
     }
 
     pub fn create_tables(&self) {
-        self.connection.execute("CREATE TABLE \"blocks\" (
-	\"id\"	INTEGER NOT NULL UNIQUE,
-	\"content\"	TEXT,
-	\"parent\"	INTEGER,
-	\"next_sibling\"	INTEGER,
-	PRIMARY KEY(\"id\" AUTOINCREMENT),
-	FOREIGN KEY(\"next_sibling\") REFERENCES \"blocks\"(\"id\") ON DELETE SET NULL,
-	FOREIGN KEY(\"parent\") REFERENCES \"blocks\"(\"id\") ON DELETE SET NULL
-);", ()).expect("failed to create table");
+        self.connection.execute(
+            "CREATE TABLE blocks (
+            id	INTEGER NOT NULL UNIQUE,
+            content	TEXT,
+            parent	INTEGER,
+            next_sibling	INTEGER,
+            PRIMARY KEY(id AUTOINCREMENT),
+            FOREIGN KEY(next_sibling) REFERENCES blocks(id) ON DELETE SET NULL,
+            FOREIGN KEY(parent) REFERENCES blocks(id) ON DELETE SET NULL);",
+            ()).expect("failed to create table");
     }
 }
